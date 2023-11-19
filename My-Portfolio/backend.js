cf856b22ff850d1152f2a374aa8e1959ea94aa58
zunheorist@gmail.com <zunheorist@gmail.com>
2023-11-19 11:25:50 +0800

    Modified Things

--- My-Portfolio/backend.js
+++ My-Portfolio/backend.js
@@ -1,3 +1,5 @@
+const SCREENAREA = document.getElementById('area');
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -6,7 +8,8 @@ function toLink(link) {
   window.location.href = link;
 }
 
-window.onload = function() {
+window.onload = async function() {
+  await sleep(1200);
   document.getElementById('loading').style.display = 'none';
   document.getElementById('screen').style.display = 'flex';
 };
